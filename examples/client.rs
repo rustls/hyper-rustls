d40ae6b7c7d23a69cc1d9dafb4ebbf4986cb63d1
Alex Crawford <alex.crawford@coreos.com>
2017-05-15 23:22:34 -0700

    update the client example

--- examples/client.rs
+++ examples/client.rs
@@ -1,60 +1,43 @@
 #![deny(warnings)]
+
+extern crate futures;
 extern crate hyper;
-
 extern crate hyper_rustls;
+extern crate tokio_core;
 
-extern crate env_logger;
-
+use futures::future::Future;
+use futures::Stream;
+use hyper::Uri;
 use std::env;
-use std::io;
-
-use hyper::Client;
-use hyper::header::Connection;
-use hyper::net::HttpConnector;
-use hyper::net::HttpsConnector;
-use hyper::client::ProxyConfig;
+use std::io::Write;
+use std::str::FromStr;
 
 fn main() {
-    env_logger::init().unwrap();
-
     let url = match env::args().nth(1) {
-        Some(url) => url,
+        Some(url) => Uri::from_str(&*url).expect("well-formed URI"),
         None => {
             println!("Usage: client <url>");
             return;
         }
     };
 
-    let tls = hyper_rustls::TlsClient::new();
+    let mut core = tokio_core::reactor::Core::new().unwrap();
+    let handle = core.handle();
+    let client = hyper::Client::configure()
+                .connector(hyper_rustls::HttpsConnector::new(4, &handle))
+                .build(&handle);
 
-    let client = match env::var("HTTP_PROXY") {
-        Ok(mut proxy) => {
-            // parse the proxy, message if it doesn't make sense
-            let mut port = 80;
-            if let Some(colon) = proxy.rfind(':') {
-                port = proxy[colon + 1..].parse().unwrap_or_else(|e| {
-                    panic!("HTTP_PROXY is malformed: {:?}, port parse error: {}", proxy, e);
-                });
-                proxy.truncate(colon);
-            }
 
-            // connector here gets us to the proxy. tls then is used for https
-            // connections via the proxy (tunnelled through the CONNECT method)
-            let connector = HttpConnector::default();
-            let proxy_config = ProxyConfig::new("http", proxy, port, connector, tls);
-            Client::with_proxy_config(proxy_config)
-        }
-        _ => {
-            let connector = HttpsConnector::new(tls);
-            Client::with_connector(connector)
-        }
-    };
-
-    let mut res = client.get(&*url)
-        .header(Connection::close())
-        .send().unwrap();
-
-    println!("Response: {}", res.status);
-    println!("Headers:\n{}", res.headers);
-    io::copy(&mut res, &mut io::stdout()).unwrap();
+    let work = client.get(url).and_then(|res| {
+        println!("Status: {}", res.status());
+        println!("Headers:\n{}", res.headers());
+        res.body().for_each(|chunk| {
+            ::std::io::stdout().write_all(&chunk)
+                .map(|_| ())
+                .map_err(From::from)
+        })
+    });
+    if let Err(err) =  core.run(work) {
+        println!("FAILED: {}", err);
+    }
 }
