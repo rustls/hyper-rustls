9f16ecb654afa42530235f500128a808495add94
Luca Bruno <lucab@debian.org>
2017-07-08 11:42:53 +0000

    lib: add rustdoc comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,30 @@
+//! # hyper-rustls
+//!
+//! A pure-Rust HTTPS connector for [hyper](https://hyper.rs), based on [Rustls](https://github.com/ctz/rustls).
+//!
+//! ## Example
+//!
+//! ```no_run
+//! extern crate hyper;
+//! extern crate hyper_rustls;
+//! extern crate tokio_core;
+//!
+//! use hyper::{Client, Uri};
+//! use tokio_core::reactor;
+//!
+//! fn main() {
+//!     let mut core = reactor::Core::new().unwrap();
+//!     let url = ("https://hyper.rs").parse().unwrap();
+//!
+//!     let client = hyper::Client::configure()
+//!         .connector(hyper_rustls::HttpsConnector::new(4, &core.handle()))
+//!         .build(&core.handle());
+//!
+//!     let res = core.run(client.get(url)).unwrap();
+//!     assert_eq!(res.status(), hyper::Ok);
+//! }
+//! ```
+
 extern crate futures;
 extern crate hyper;
 extern crate rustls;
