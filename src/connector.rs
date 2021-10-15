5ff82f0f0e0683073259668067a21a1bae4bb824
Gabriel de Perthuis <g2p.code@gmail.com>
2021-10-15 17:59:17 +0200

    Add HttpsConnectorBuilder
    This gives more control over various rustls features,
    as well as ensures that enabling connector features
    like http1/http2 can only be done when the appropriate
    crate features are enabled.

--- src/connector.rs
+++ src/connector.rs
@@ -5,14 +5,13 @@ use std::sync::Arc;
 use std::task::{Context, Poll};
 use std::{fmt, io};
 
-#[cfg(feature = "tokio-runtime")]
-use hyper::client::connect::HttpConnector;
 use hyper::{client::connect::Connection, service::Service, Uri};
-use rustls::ClientConfig;
 use tokio::io::{AsyncRead, AsyncWrite};
 use tokio_rustls::TlsConnector;
 
 use crate::stream::MaybeHttpsStream;
+
+pub mod builder;
 
 type BoxError = Box<dyn std::error::Error + Send + Sync>;
 
@@ -21,20 +20,7 @@ type BoxError = Box<dyn std::error::Error + Send + Sync>;
 pub struct HttpsConnector<T> {
     force_https: bool,
     http: T,
-    tls_config: Arc<ClientConfig>,
-}
-
-#[cfg(all(
-    any(feature = "rustls-native-certs", feature = "webpki-roots"),
-    feature = "tokio-runtime"
-))]
-impl HttpsConnector<HttpConnector> {
-    /// Force the use of HTTPS when connecting.
-    ///
-    /// If a URL is not `https` when connecting, an error is returned. Disabled by default.
-    pub fn https_only(&mut self, enable: bool) {
-        self.force_https = enable;
-    }
+    tls_config: Arc<rustls::ClientConfig>,
 }
 
 impl<T> fmt::Debug for HttpsConnector<T> {
@@ -47,7 +33,7 @@ impl<T> fmt::Debug for HttpsConnector<T> {
 
 impl<H, C> From<(H, C)> for HttpsConnector<H>
 where
-    C: Into<Arc<ClientConfig>>,
+    C: Into<Arc<rustls::ClientConfig>>,
 {
     fn from((http, cfg): (H, C)) -> Self {
         HttpsConnector {
@@ -81,38 +67,43 @@ where
     }
 
     fn call(&mut self, dst: Uri) -> Self::Future {
-        let is_https = dst.scheme_str() == Some("https");
+        // dst.scheme() would need to derive Eq to be matchable;
+        // use an if cascade instead
+        if let Some(sch) = dst.scheme() {
+            if sch == &http::uri::Scheme::HTTP && !self.force_https {
+                let connecting_future = self.http.call(dst);
 
-        if !is_https && self.force_https {
-            // Early abort if HTTPS is forced but can't be used
-            let err = io::Error::new(io::ErrorKind::Other, "https required but URI was not https");
-            Box::pin(async move { Err(err.into()) })
-        } else if !is_https {
-            let connecting_future = self.http.call(dst);
+                let f = async move {
+                    let tcp = connecting_future.await.map_err(Into::into)?;
 
-            let f = async move {
-                let tcp = connecting_future.await.map_err(Into::into)?;
+                    Ok(MaybeHttpsStream::Http(tcp))
+                };
+                Box::pin(f)
+            } else if sch == &http::uri::Scheme::HTTPS {
+                let cfg = self.tls_config.clone();
+                let hostname = dst.host().unwrap_or_default().to_string();
+                let connecting_future = self.http.call(dst);
 
-                Ok(MaybeHttpsStream::Http(tcp))
-            };
-            Box::pin(f)
+                let f = async move {
+                    let tcp = connecting_future.await.map_err(Into::into)?;
+                    let connector = TlsConnector::from(cfg);
+                    let dnsname = rustls::ServerName::try_from(hostname.as_str())
+                        .map_err(|_| io::Error::new(io::ErrorKind::Other, "invalid dnsname"))?;
+                    let tls = connector
+                        .connect(dnsname, tcp)
+                        .await
+                        .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+                    Ok(MaybeHttpsStream::Https(tls))
+                };
+                Box::pin(f)
+            } else {
+                let err =
+                    io::Error::new(io::ErrorKind::Other, format!("Unsupported scheme {}", sch));
+                Box::pin(async move { Err(err.into()) })
+            }
         } else {
-            let cfg = self.tls_config.clone();
-            let hostname = dst.host().unwrap_or_default().to_string();
-            let connecting_future = self.http.call(dst);
-
-            let f = async move {
-                let tcp = connecting_future.await.map_err(Into::into)?;
-                let connector = TlsConnector::from(cfg);
-                let dnsname = rustls::ServerName::try_from(hostname.as_str())
-                    .map_err(|_| io::Error::new(io::ErrorKind::Other, "invalid dnsname"))?;
-                let tls = connector
-                    .connect(dnsname, tcp)
-                    .await
-                    .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
-                Ok(MaybeHttpsStream::Https(tls))
-            };
-            Box::pin(f)
+            let err = io::Error::new(io::ErrorKind::Other, "Missing scheme");
+            Box::pin(async move { Err(err.into()) })
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,33 @@
 //! # hyper-rustls
 //!
-//! A pure-Rust HTTPS connector for [hyper](https://hyper.rs), based on [Rustls](https://github.com/ctz/rustls).
+//! A pure-Rust HTTPS connector for [hyper](https://hyper.rs), based on
+//! [Rustls](https://github.com/ctz/rustls).
 //!
 //! ## Example
 //!
 //! ```no_run
-//! # #[cfg(all(feature = "rustls-native-certs", feature = "tokio-runtime"))]
+//! # #[cfg(all(feature = "rustls-native-certs", feature = "tokio-runtime", feature = "http1"))]
 //! # fn main() {
 //! use hyper::{Body, Client, StatusCode, Uri};
 //!
 //! let mut rt = tokio::runtime::Runtime::new().unwrap();
 //! let url = ("https://hyper.rs").parse().unwrap();
-//! let https = hyper_rustls::HttpsConnector::with_native_roots();
+//! let https = hyper_rustls::HttpsConnectorBuilder::new()
+//!     .with_native_roots()
+//!     .https_only()
+//!     .enable_http1()
+//!     .build();
 //!
 //! let client: Client<_, hyper::Body> = Client::builder().build(https);
 //!
 //! let res = rt.block_on(client.get(url)).unwrap();
 //! assert_eq!(res.status(), StatusCode::OK);
 //! # }
-//! # #[cfg(not(all(feature = "rustls-native-certs", feature = "tokio-runtime")))]
+//! # #[cfg(not(all(feature = "rustls-native-certs", feature = "tokio-runtime", feature = "http1")))]
 //! # fn main() {}
 //! ```
 
+#![warn(missing_docs)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
 mod config;
@@ -29,5 +35,15 @@ mod connector;
 mod stream;
 
 pub use crate::config::ConfigBuilderExt;
+pub use crate::connector::builder::ConnectorBuilder as HttpsConnectorBuilder;
 pub use crate::connector::HttpsConnector;
 pub use crate::stream::MaybeHttpsStream;
+
+/// The various states of the [`HttpsConnectorBuilder`]
+pub mod builderstates {
+    #[cfg(feature = "http2")]
+    pub use crate::connector::builder::WantsProtocols3;
+    pub use crate::connector::builder::{
+        WantsProtocols1, WantsProtocols2, WantsSchemes, WantsTlsConfig,
+    };
+}
